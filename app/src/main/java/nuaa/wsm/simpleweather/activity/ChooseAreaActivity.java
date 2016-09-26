78011afb7af5775f24fc40dcb4a6d438af3cfb90
Fear <pdsrazor@gmail.com>
2016-09-26 22:52:21 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/nuaa/wsm/simpleweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/nuaa/wsm/simpleweather/activity/ChooseAreaActivity.java
@@ -68,14 +68,22 @@ public class ChooseAreaActivity extends Activity {
      */
     private int currentLevel;
 
+    /**
+     * 是否从WeatherActivity中跳转过来。
+     */
+    private boolean isFromWeatherActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+
         SharedPreferences prefs = PreferenceManager.
                 getDefaultSharedPreferences(this);
-        if (prefs.getBoolean("city_selected", false)) {
+        if (prefs.getBoolean("city_selected", false)
+                && !isFromWeatherActivity) {// 已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳转到
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
             finish();
@@ -259,6 +267,10 @@ public class ChooseAreaActivity extends Activity {
         } else if (currentLevel == LEVEL_CITY) {
             queryProvinces();
         } else {
+            if (isFromWeatherActivity) {
+                Intent intent = new Intent(this, WeatherActivity.class);
+                startActivity(intent);
+            }
             finish();
         }
     }
--- app/src/main/java/nuaa/wsm/simpleweather/activity/WeatherActivity.java
+++ app/src/main/java/nuaa/wsm/simpleweather/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package nuaa.wsm.simpleweather.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -69,8 +70,8 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         temp1Text = (TextView) findViewById(R.id.temp1);
         temp2Text = (TextView) findViewById(R.id.temp2);
         currentDateText = (TextView) findViewById(R.id.current_date);
-        //switchCity = (Button) findViewById(R.id.switch_city);
-        //refreshWeather = (Button) findViewById(R.id.refresh_weather);
+        switchCity = (Button) findViewById(R.id.switch_city);
+        refreshWeather = (Button) findViewById(R.id.refresh_weather);
         String countyCode = getIntent().getStringExtra("county_code");
         if (!TextUtils.isEmpty(countyCode)) {
             // 有县级代号时就去查询天气
@@ -82,14 +83,14 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
             // 没有县级代号时就直接显示本地天气
             showWeather();
         }
-       // switchCity.setOnClickListener(this);
-       // refreshWeather.setOnClickListener(this);
+        switchCity.setOnClickListener(this);
+        refreshWeather.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            /*
+
             case R.id.switch_city:
                 Intent intent = new Intent(this, ChooseAreaActivity.class);
                 intent.putExtra("from_weather_activity", true);
@@ -104,7 +105,7 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
                 if (!TextUtils.isEmpty(weatherCode)) {
                     queryWeatherInfo(weatherCode);
                 }
-                break;*/
+                break;
             default:
                 break;
         }
